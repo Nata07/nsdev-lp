550bb9ece273e9140a8e10e9688bf899a5b0a745
Natanael <nata.mw@hotmail.com>
2024-03-22 23:36:42 -0300

    feat: changed metada SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,20 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
-import { DefaultSEO } from "./default-seo";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "NSDEV Creative",
   description: "Impulsione seu negócio com estratégia, design e tecnologia",
+  openGraph: {
+    images: [{
+      type: 'website',
+      url: 'https://nsdev.vercel.app/logo.svg',
+      width: 800,
+      height: 600
+    }]
+  }
 };
 
 export default function RootLayout({
